0ffd3a6a80afd5bbba5284fabe98cd33dac061b9
nayounge <34162358+nayounge@users.noreply.github.com>
2017-12-13 04:20:40 +0900

    Add files via upload

--- bean/Provider.java
+++ bean/Provider.java
@@ -3,7 +3,7 @@ package bean;
 public interface Provider {
 String DRIVER="oracle.jdbc.driver.OracleDriver";
 String CONNECTION_URL="jdbc:oracle:thin:@localhost:1521:oraknu";
-String USERNAME="kdhong";
-String PASSWORD="kdhong";
+String USERNAME="sys as sysdba";
+String PASSWORD="DBLab012345";
 
 }
